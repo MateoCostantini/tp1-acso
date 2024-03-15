b9c411846a2e63cda346ca3cfe9f53cddeb34229
Mateo Costantini <costantinim@udesa.edu.ar>
2024-03-14 23:41:06 -0300

    Pasar a binario en el decode funcionando

--- src/sim.c
+++ src/sim.c
@@ -1,42 +1,45 @@
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 #include <string.h>
 
-void process_instruction()
-{
-    /* execute one instruction here. You should use CURRENT_STATE and modify
-     * values in NEXT_STATE. You can call mem_read_32() and mem_write_32() to
-     * access memory. 
-     * 
-     * Sugerencia: hagan una funcion para decode()
-     *             y otra para execute()
-     * 
-     * */
-
+void process_instruction() {
+  /* execute one instruction here. You should use CURRENT_STATE and modify
+   * values in NEXT_STATE. You can call mem_read_32() and mem_write_32() to
+   * access memory.
+   *
+   * Sugerencia: hagan una funcion para decode()
+   *             y otra para execute()
+   *
+   * */
 }
 
 
-void decode(char* instruction){
-    int potencias = {8, 4, 2, 1, 0};
-    for (size_t i = 0; i < 8; i++){
-        char character = instruction[i];
-        int digit;
-        if (strcmp(character,'a') == 0){
-            digit = 10;
-        } else if (strcmp(character,'b') == 0){
-            digit = 11;
-        } else if (strcmp(character,'c') == 0){
-            digit = 12;
-        } else if (strcmp(character,'d') == 0){
-            digit = 13;
-        } else if (strcmp(character,'e') == 0){
-            digit = 14;
-        } else if (strcmp(character,'f') == 0){
-            digit = 15;
-        } else{
-            digit = (int) character;
-        }
-
-        //for (size_t j = 0; )
+int* decode(char *instruction) {
+  int *binary_instruction = malloc(32*sizeof(int));
+  //if binary_instruction == NULL ...
+  for (size_t i = 0; i < 8; i++) {
+    char digit_hex = instruction[i];
+    int digit_dec;
+    if (digit_hex == 'a') {
+      digit_dec = 10;
+    } else if (digit_hex == 'b') {
+      digit_dec = 11;
+    } else if (digit_hex == 'c') {
+      digit_dec = 12;
+    } else if (digit_hex == 'd') {
+      digit_dec = 13;
+    } else if (digit_hex == 'e') {
+      digit_dec = 14;
+    } else if (digit_hex == 'f') {
+      digit_dec = 15;
+    } else {
+      digit_dec = (int)digit_hex;
     }
+    for (size_t j = 0; j < 4; j++) {
+      int bit = digit_dec % 2;
+      digit_dec = digit_dec/2;
+      binary_instruction[(3 - j) + i * 4] = bit;
+    }
+  }
+  return binary_instruction;
 }
