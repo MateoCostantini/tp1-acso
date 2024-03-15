f8336b3227cd9b43f56dcf14310bba61d4836c16
Mateo Costantini <costantinim@udesa.edu.ar>
2024-03-14 21:03:24 -0300

    Empezamos con la funcion decode()

--- src/sim.c
+++ src/sim.c
@@ -14,3 +14,29 @@ void process_instruction()
      * */
 
 }
+
+
+void decode(char* instruction){
+    int potencias = {8, 4, 2, 1, 0};
+    for (size_t i = 0; i < 8; i++){
+        char character = instruction[i];
+        int digit;
+        if (strcmp(character,'a') == 0){
+            digit = 10;
+        } else if (strcmp(character,'b') == 0){
+            digit = 11;
+        } else if (strcmp(character,'c') == 0){
+            digit = 12;
+        } else if (strcmp(character,'d') == 0){
+            digit = 13;
+        } else if (strcmp(character,'e') == 0){
+            digit = 14;
+        } else if (strcmp(character,'f') == 0){
+            digit = 15;
+        } else{
+            digit = (int) character;
+        }
+
+        //for (size_t j = 0; )
+    }
+}
