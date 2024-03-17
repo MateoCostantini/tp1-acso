ef339e716fcf815a952c078ff4f539d6ee0faf00
Gonzalo Lewit <gonzalewit@Gonzalos-MacBook-Pro.local>
2024-03-17 18:53:28 -0300

    agrego nuevo archivo para no modificar instructions.c

---
+++ src/instructions_calloc.c
@@ -0,0 +1,388 @@
+#include <stdio.h>
+
+// agergar al makefile
+// falta tambien definir el length del opcode para cada instruccion
+
+typedef struct {
+  const int *opcode;
+  const int *cond;
+  const int *imms;
+  int opcode_length;
+  void (*instruction_function)(int *);
+  int *(*identify_params)(int *);
+} Instruction;
+
+// me agarro la duda si la forma de acceder a los registros y memoria es con int
+// binario y no decimal!!
+
+// Hay funciones que se repiten (son iguales) habria que sacar las repetidas y
+// generalizar
+
+// habria que inicializar params como arreglos de 0s xq sino sumaria cualquier cosa
+
+int *identify_params_1(
+    int *instruction_base) { // chequear si el opction y imm3 hay que ponerlo
+                             // como param
+  // sirve para ADDS_ER, SUBS_ER, ANDS_SR, EOR_SR, ORR_SR
+  int *params = (int *)calloc(3, sizeof(int));
+  if (params == NULL) {
+    return NULL;
+  }
+  for (size_t i=0; i < 32; i++) {
+    if (i >= (31 - 20) && i <= (31 - 16)) {
+      params[0] += instruction_base[i] * (pow(2, (31 - 16) - i));
+    } else if (i >= (31 - 9) && i <= (31 - 5)) {
+      params[1] += instruction_base[i] * (pow(2, (31 - 5) - i));
+    } else if (i >= (31 - 4) && i <= 31) {
+      params[2] += instruction_base[i] * (pow(2, 31 - i));
+    }
+  }
+  return params;
+}
+
+int *identify_params_2(int *instruction_base) {
+  // sirve para ADDS_I, SUBS_I
+  int *params = (int *)calloc(4, sizeof(int));
+  if (params == NULL) {
+    return NULL;
+  }
+  for (size_t i=0; i < 32; i++) {
+    if ((31 - 23) <= i <= (31 - 22)) {
+      params[0] += instruction_base[i] * (pow(2, (31 - 22) - i));
+    } else if ((31 - 21) <= i <= (31 - 10)) {
+      params[1] += instruction_base[i] * (pow(2, (31 - 10) - i));
+    } else if ((31 - 9) <= i <= (31 - 5)) {
+      params[2] += instruction_base[i] * (pow(2, (31 - 5) - i));
+    } else if ((31 - 4) <= i <= 31) {
+      params[3] += instruction_base[i] * (pow(2, (31 - i)));
+    }
+  }
+  return params;
+}
+
+int *identify_params_HLT(int *instruction_base) {
+  int *params = (int *)calloc(1, sizeof(int));
+  if (params == NULL) {
+    return NULL;
+  }
+  for (size_t i=0; i < 32; i++) {
+    if ((31 - 20) <= i <= (31 - 5)) {
+      params[0] += instruction_base[i] * (pow(2, (31 - 5) - i));
+    }
+  }
+  return params;
+}
+
+int *identify_params_CMP_ER(int *instruction_base) {
+  int *params = (int *)calloc(2, sizeof(int));
+  if (params == NULL) {
+    return NULL;
+  }
+  for (size_t i=0; i < 32; i++) {
+    if ((31 - 20) <= i <= (31 - 16)) {
+      params[0] += instruction_base[i] * (pow(2, (31 - 16) - i));
+    } else if ((31 - 9) <= i <= (31 - 5)) {
+      params[1] += instruction_base[i] * (pow(2, (31 - 5) - i));
+    }
+  }
+  return params;
+}
+
+int *identify_params_CMP_I(int *instruction_base) {
+  int *params = (int *)calloc(3, sizeof(int));
+  if (params == NULL) {
+    return NULL;
+  }
+  for (size_t i=0; i < 32; i++) {
+    if ((31 - 23) <= i <= (31 - 22)) {
+      params[0] += instruction_base[i] * (pow(2, (31 - 22) - i));
+    } else if ((31 - 21) <= i <= (31 - 10)) {
+      params[1] += instruction_base[i] * (pow(2, (31 - 10) - i));
+    } else if ((31 - 9) <= i <= (31 - 5)) {
+      params[2] += instruction_base[i] * (pow(2, (31 - 5) - i));
+    }
+  }
+  return params;
+}
+
+int *identify_params_B(int *instruction_base) {
+  int *params = (int *)calloc(1, sizeof(int));
+  if (params == NULL) {
+    return NULL;
+  }
+  for (size_t i=0; i < 32; i++) {
+    if ((31 - 25) <= i <= 31) {
+      params[0] += instruction_base[i] * (pow(2, 31 - i));
+    }
+  }
+  return params;
+}
+
+int *identify_params_BR(int *instruction_base) {
+  int *params = (int *)calloc(1, sizeof(int));
+  if (params == NULL) {
+    return NULL;
+  }
+  for (size_t i=0; i < 32; i++) {
+    if ((31 - 9) <= i <= (31 - 5)) {
+      params[0] += instruction_base[i] * (pow(2, (31 - 5) - i));
+    }
+  }
+  return params;
+}
+
+int *identify_params_Bcond(int *instruction_base) { // muchas dudas con este
+  int *params = (int *)calloc(1, sizeof(int));
+  if (params == NULL) {
+    return NULL;
+  }
+  for (size_t i=0; i < 32; i++) {
+    if ((31 - 3) <= i <= 31) {
+      params[0] += instruction_base[i] * (pow(2, 31 - i));
+    }
+  }
+  return params;
+}
+
+int *identify_params_3(int *instruction_base){
+  // sirve para LSR, LSL, STUR, STURB, STURH, LDUR, LDURH, LDURB
+  int *params = (int *)calloc(2, sizeof(int));
+  if (params == NULL) {
+    return NULL;
+  }
+  for (size_t i=0; i < 32; i++) {
+    if ((31 - 9) <= i <= (31 - 5)) {
+      params[0] += instruction_base[i] * (pow(2, (31 - 5) - i));
+    } else if ((31 - 4) <= i <= 31) {
+      params[1] += instruction_base[i] * (pow(2, 31 - i));
+    }
+  }
+  return params;
+}
+
+int *identify_params_MOVZ(int *instruction_base) {
+  int params[1];
+  for (size_t i=0; i < 32; i++) {
+    if ((31 - 4) <= i <= 31) {
+      params[0] += instruction_base[i] * (pow(2, 31 - i));
+    }
+  }
+  return params;
+}
+
+
+
+// definir cada instruccion con su opcode
+void build_instructions() {
+  const int instructions_quantity =
+      27; // la defini tambien en sim.c, identify_instruction
+  Instruction *instructions[27];
+  // tal vez en vez de hacerlo malloc hacerlo estatico total en cada posicion
+  // siempre se mantiene el mismo punetro a struct
+  Instruction *ADDS_ER;
+  const int op[] = {1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1};
+  ADDS_ER->opcode = op;
+  ADDS_ER->identify_params = identify_params_1;
+  // agregar las funciones para esa instruccion en los parametros de la struct
+  instructions[0] = ADDS_ER;
+
+  Instruction *ADDS_I;
+  const int op[] = {1, 0, 1, 1, 0, 0, 0, 1};
+  ADDS_I->opcode = op;
+  // agregar funciones
+  ADDS_I->identify_params = identify_params_2;
+  instructions[1] = ADDS_I;
+
+  Instruction *SUBS_ER;
+  const int op[] = {1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1};
+  SUBS_ER->opcode = op;
+  SUBS_ER->identify_params = identify_params_1;
+  // agregar funciones
+  instructions[2] = SUBS_ER;
+
+  Instruction *SUBS_I;
+  const int op[] = {1, 1, 1, 1, 0, 0, 0, 1};
+  SUBS_I->opcode = op;
+  SUBS_I->identify_params = identify_params_2;
+  // agregar funciones
+  instructions[3] = SUBS_I;
+
+  Instruction *HLT;
+  const int op[] = {1, 1, 0, 1, 0, 1,
+                    0, 0, 0, 1, 0}; // chequear si falta un 0 por la consigna
+  HLT->opcode = op;
+  HLT->identify_params = identify_params_HLT;
+  // agregar funciones
+  instructions[4] = HLT;
+
+  Instruction *CMP_ER;
+  const int op[] = {1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1};
+  CMP_ER->opcode = op;
+  CMP_ER->identify_params = identify_params_CMP_ER;
+  // agregar funciones
+  instructions[5] = CMP_ER;
+
+  Instruction *CMP_I;
+  const int op[] = {1, 1, 1, 1, 0, 0, 0, 1};
+  CMP_I->opcode = op;
+  CMP_I->identify_params = identify_params_CMP_I;
+  // agregar funciones
+  instructions[6] = CMP_I;
+
+  Instruction *ANDS_SR;
+  const int op[] = {1, 1, 1, 0, 1, 0, 1, 0};
+  ANDS_SR->opcode = op;
+  ANDS_SR->identify_params = identify_params_1;
+  // agregar funciones
+  instructions[7] = ANDS_SR;
+
+  Instruction *EOR_SR;
+  const int op[] = {1, 1, 0, 0, 1, 0, 1, 0};
+  EOR_SR->opcode = op;
+  EOR_SR->identify_params = identify_params_1;
+  // agregar funciones
+  instructions[8] = EOR_SR;
+
+  Instruction *ORR_SR;
+  const int op[] = {1, 0, 1, 0, 1, 0, 1, 0};
+  ORR_SR->opcode = op;
+  ORR_SR->identify_params = identify_params_1;
+  // agregar funciones
+  instructions[9] = ORR_SR;
+
+  Instruction *B;
+  const int op[] = {0, 0, 0, 1, 0, 1};
+  B->opcode = op;
+  B->identify_params = identify_params_B;
+  // agregar funciones
+  instructions[10] = B;
+
+  Instruction *BR;
+  const int op[] = {1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0,
+                    1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0};
+  BR->opcode = op;
+  BR->identify_params = identify_params_BR;
+  // agregar funciones
+  instructions[11] = BR;
+
+  Instruction *BEQ;
+  const int op[] = {0, 1, 0, 1, 0, 1, 0, 0};
+  const int cond_[] = {0, 0, 0, 0};
+  BEQ->opcode = op;
+  BEQ->cond = cond_;
+  BEQ->identify_params = identify_params_Bcond;
+  // agregar funciones
+  instructions[12] = BEQ;
+
+  Instruction *BNE;
+  const int op[] = {0, 1, 0, 1, 0, 1, 0, 0};
+  const int cond_[] = {0, 0, 0, 1};
+  BNE->opcode = op;
+  BNE->cond = cond_;
+  BNE->identify_params = identify_params_Bcond;
+  // agregar funciones
+  instructions[13] = BNE;
+
+  Instruction *BGT;
+  const int op[] = {0, 1, 0, 1, 0, 1, 0, 0};
+  const int cond_[] = {1, 1, 0, 0};
+  BGT->opcode = op;
+  BGT->cond = cond_;
+  BGT->identify_params = identify_params_Bcond;
+  // agregar funciones
+  instructions[14] = BGT;
+
+  Instruction *BLT;
+  const int op[] = {0, 1, 0, 1, 0, 1, 0, 0};
+  const int cond_[] = {1, 0, 1, 1};
+  BLT->opcode = op;
+  BLT->cond = cond_;
+  BLT->identify_params = identify_params_Bcond;
+  // agregar funciones
+  instructions[15] = BLT;
+
+  Instruction *BGE;
+  const int op[] = {0, 1, 0, 1, 0, 1, 0, 0};
+  const int cond_[] = {1, 0, 1, 0};
+  BGE->opcode = op;
+  BGE->cond = cond_;
+  BGE->identify_params = identify_params_Bcond;
+  // agregar funciones
+  instructions[16] = BGE;
+
+  Instruction *BLE;
+  const int op[] = {0, 1, 0, 1, 0, 1, 0, 0};
+  const int cond_[] = {1, 1, 0, 1};
+  BLE->opcode = op;
+  BLE->cond = cond_;
+  BLE->identify_params = identify_params_Bcond;
+  // agregar funciones
+  instructions[17] = BLE;
+
+  Instruction *LSL_I;
+  const int op[] = {1, 1, 0, 1, 0, 0, 1, 1, 0};
+  const int imms_[] = {1, 1, 1, 1, 1, 1};
+  LSL_I->opcode = op;
+  LSL_I->imms = imms_;
+  LSL_I->identify_params = identify_params_3;
+  // agregar funciones
+  instructions[18] = LSL_I;
+
+  Instruction *LSR_I;
+  const int op[] = {1, 1, 0, 1, 0, 0, 1, 1, 0}; // no pusimos N
+  const int imms_[] = {0, 0, 0, 0, 0, 0};
+  LSR_I->opcode = op;
+  LSR_I->imms = imms_;
+  LSR_I->identify_params = identify_params_3;
+  // agregar funciones
+  instructions[19] = LSR_I;
+
+  Instruction *STUR;
+  const int op[] = {1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0};
+  STUR->opcode = op;
+  STUR->identify_params = identify_params_3;
+  // agregar funciones
+  instructions[20] = STUR;
+
+  Instruction *STURB;
+  const int op[] = {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0};
+  STURB->opcode = op;
+  STURB->identify_params = identify_params_3;
+  // agregar funciones
+  instructions[21] = STURB;
+
+  Instruction *STURH;
+  const int op[] = {0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0};
+  STURH->opcode = op;
+  STURH->identify_params = identify_params_3;
+  // agregar funciones
+  instructions[22] = STURH;
+
+  Instruction *LDUR;
+  const int op[] = {1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0};
+  LDUR->opcode = op;
+  LDUR->identify_params = identify_params_3;
+  // agregar funciones
+  instructions[23] = LDUR;
+
+  Instruction *LDURH;
+  const int op[] = {0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0};
+  LDURH->opcode = op;
+  LDURH->identify_params = identify_params_3;
+  // agregar funciones
+  instructions[24] = LDURH;
+
+  Instruction *LDURB;
+  const int op[] = {0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0};
+  LDURB->opcode = op;
+  LDURB->identify_params = identify_params_3;
+  // agregar funciones
+  instructions[25] = LDURB;
+
+  Instruction *MOVZ;
+  const int op[] = {1, 1, 0, 1, 0, 0, 1, 0, 1};
+  MOVZ->opcode = op;
+  MOVZ->identify_params = identify_params_MOVZ;
+  // agregar funciones
+  instructions[26] = MOVZ;
+}
